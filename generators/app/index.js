6bd7ddcabddab4e12cc69ed55fec0e23c97dd4d5
Thomas van der Velde <contact@toolbarthomas.net>
2017-07-20 17:21:17 +0200

    Generator logic

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,6 +2,8 @@
 const Generator = require('yeoman-generator');
 const Chalk = require('chalk');
 const fse = require('fs-extra');
+const path = require('path');
+const replace = require('replace');
 
 const Basepath = './src/resources';
 
@@ -16,14 +18,38 @@ class Totem extends Generator {
             case 'page':
                 output_config['base_folder'] = 'pages';
                 output_config['success_message'] = 'Page created';
+                output_config['base_files'] = [
+                    'stylesheets/page.scss',
+                    'javascripts/page.js'
+                ];
+                output_config['callback'] = function(src, name) {
+                    var file = src + '/index.twig';
+
+                    replace({
+                        regex: "__PAGE__",
+                        replacement: name,
+                        paths: [file],
+                        recursive: true,
+                        silent: true,
+                    });
+                }
                 break;
             case 'template':
                 output_config['base_folder'] = 'templates';
                 output_config['success_message'] = 'Template created';
+                output_config['base_files'] = [
+                    'stylesheets/template.scss',
+                    'javascripts/template.js',
+                    'template.twig'
+                ];
                 break;
             default:
                 output_config['base_folder'] = 'modules';
                 output_config['success_message'] = 'Module created';
+                output_config['base_files'] = [
+                    'stylesheets/module.scss',
+                    'javascripts/module.js'
+                ];
         }
 
         return output_config;
@@ -38,7 +64,7 @@ module.exports = class extends Totem  {
     this.log(Chalk.reset('For more information: https://github.com/toolbarthomas/generator-totem') + '\n');
 
     const prompts = [
-{
+    {
             type: 'list',
             name: 'section_type',
             message: 'For which type do you wan\'t to define your new module in?',
@@ -66,16 +92,54 @@ module.exports = class extends Totem  {
   }
 
   writing() {
-    this.output_config = this.getOutputConfig(this.props.section_type);
+    var module_name = this.props.module_name;
+    var output_config = this.getOutputConfig(this.props.section_type);
 
+    var dest = Basepath + '/' + output_config.base_folder + '/' + module_name;
+
+    // Create file structure for the selected type
     fse.copy(
-        this.templatePath(this.output_config.section),
-        this.destinationPath(Basepath + '/' + this.output_config.base_folder + '/' + this.props.module_name)
+        this.templatePath(output_config.section),
+        this.destinationPath(dest)
     ).then(() => {
+        this.log(Chalk.yellow('Structure created, creating files...'));
+
+        if(typeof output_config.base_files === 'undefined') {
+            return;
+        }
+
+        var i = 0;
+        output_config.base_files.forEach(function(base_file) {
+            var rename = {
+                input: dest + '/' + base_file,
+                output: path.dirname(dest + '/' + base_file) + '/' + this.props.module_name + '.' + (path.extname(base_file).split('.').pop())
+            };
+
+            // Rename the base files
+            fse.rename(rename.input, rename.output, function(error) {
+                i++;
+
+                if(error) {
+                    throw error;
+                }
+
+                // Init the callback when all files have been renamed
+                if (i < (output_config.base_files.length)) {
+                    return;
+                }
+
+                if (typeof output_config.callback != 'function')
+                {
+                    return;
+                }
+
+                output_config.callback(dest, module_name);
+            });
+        }, this);
+
         this.log(Chalk.green(output_config.success_message));
-    })
-        .catch(error => {
-            this.log(Chalk.red('An error has occured: ' + error));
-        });
+    }).catch(error => {
+        this.log(Chalk.red('An error has occured: ' + error));
+     });
   }
 };
