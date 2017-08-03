13c10c9c9736316c521ca4b9ed6a7229555690f1
Thomas van der Velde <contact@toolbarthomas.net>
2017-08-03 11:48:49 +0200

    Template structure
    - Rename extends and document locations
    - Prompt extra question to bind a template on a page
    - Cleanup

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,13 +8,13 @@ const replace = require('replace-in-file');
 const Basepath = './src/resources';
 
 class Totem extends Generator {
-    getOutputConfig(section_type) {
+    getOutputConfig(category) {
         var output_config = [];
 
-        output_config['section'] = section_type;
+        output_config['category'] = category;
 
         // Define the base folder to place the new module in.
-        switch(section_type) {
+        switch(category) {
             case 'page':
                 output_config['base_folder'] = 'pages';
                 output_config['success_message'] = 'Page created';
@@ -22,15 +22,25 @@ class Totem extends Generator {
                     'stylesheets/page.scss',
                     'javascripts/page.js'
                 ];
-                output_config['callback'] = function(src, name) {
+                output_config['callback'] = function(src, labels) {
+                    if(typeof labels.title === 'undefined' || labels.template === 'undefined' ) {
+                        return;
+                    }
+
                     var files = [
                         src + '/index.twig'
                     ];
 
                     replace({
-                        from: [/__PAGE__/g],
-                        to: name,
                         files: files,
+                        from: [
+                            /__PAGE__/g,
+                            /__TEMPLATE__/g
+                        ],
+                        to: [
+                            labels.title,
+                            labels.template
+                        ],
                         encoding: 'utf8'
                     });
                 }
@@ -43,6 +53,22 @@ class Totem extends Generator {
                     'javascripts/template.js',
                     'template.twig'
                 ];
+                output_config['callback'] = function (src, labels) {
+                    if (typeof labels.title === 'undefined') {
+                        return;
+                    }
+
+                    var files = [
+                        src + '/' + labels.title + '.twig'
+                    ];
+
+                    replace({
+                        files: files,
+                        from: [/__TEMPLATE__/g],
+                        to: labels.title,
+                        encoding: 'utf8'
+                    });
+                }
                 break;
             default:
                 output_config['base_folder'] = 'modules';
@@ -51,15 +77,19 @@ class Totem extends Generator {
                     'stylesheets/module.scss',
                     'javascripts/module.js'
                 ];
-                output_config['callback'] = function (src, name) {
+                output_config['callback'] = function (src, labels) {
+                    if(typeof labels.title === 'undefined') {
+                        return;
+                    }
+
                     var files = [
-                        src + '/stylesheets/' + name +'.scss'
+                        src + '/stylesheets/' + labels.title +'.scss'
                     ];
 
                     replace({
-                        from: [/__MODULE__/g],
-                        to: name,
                         files: files,
+                        from: [/__MODULE__/g],
+                        to: labels.title,
                         encoding: 'utf8'
                     });
                 }
@@ -72,26 +102,37 @@ module.exports = class extends Totem  {
   prompting() {
     this.log('');
     this.log(Chalk.green('Welcome to Totem module generator.') + '\n');
-    this.log(Chalk.green('This setup will let you create a new module within a specifc section') + '\n');
-    this.log(Chalk.reset('For more information: https://github.com/toolbarthomas/generator-totem') + '\n');
+    this.log(Chalk.green('This setup will let you create a new module within the specified category') + '\n');
+    this.log(Chalk.reset('See: https://github.com/toolbarthomas/generator-totem') + '\n');
 
     const prompts = [
     {
             type: 'list',
-            name: 'section_type',
-            message: 'For which type do you wan\'t to define your new module in?',
+            name: 'category',
+            message: 'For which category do you wan\'t to define your partial in?',
             choices: [
                 'module',
                 'page',
                 'template'
             ],
-            default: 0
+            default: 0,
         },
         {
             type: 'input',
-            name: 'module_name',
-            message: 'Name your module',
-            default: 'new-module'
+            name: 'title',
+            message: 'Name your partial.',
+            default: 'new',
+            filter: function(name) {
+                return name = name.replace(' ','-');
+            }
+        },
+        {
+            when: function(response) {
+                return response.category == 'page';
+            },
+            name: 'template',
+            message: 'For wich template is this page meant for?',
+            default: 'default'
         }
     ];
 
@@ -99,19 +140,22 @@ module.exports = class extends Totem  {
         // To access props later use this.props.someAnswer;
         this.props = props;
 
-        this.log(Chalk.green('Affirmative, ready to setup ' + this.props.module_name))
+        this.log(Chalk.green('Affirmative, ready to setup ' + this.props.title))
     });
   }
 
   writing() {
-    var module_name = this.props.module_name;
-    var output_config = this.getOutputConfig(this.props.section_type);
+    var category = this.props.category;
+    var title = this.props.title;
+    var template = this.props.template;
 
-    var dest = Basepath + '/' + output_config.base_folder + '/' + module_name;
+    var output_config = this.getOutputConfig(this.props.category);
+
+    var dest = Basepath + '/' + output_config.base_folder + '/' + title;
 
     // Create file structure for the selected type
     fse.copy(
-        this.templatePath(output_config.section),
+        this.templatePath(output_config.category),
         this.destinationPath(dest)
     ).then(() => {
         this.log(Chalk.yellow('Structure created, creating files...'));
@@ -120,32 +164,36 @@ module.exports = class extends Totem  {
             return;
         }
 
-        var i = 0;
+        // Queuer to fire up the callback
+        var queue = 0;
         output_config.base_files.forEach(function(base_file) {
             var rename = {
                 input: dest + '/' + base_file,
-                output: path.dirname(dest + '/' + base_file) + '/' + this.props.module_name + '.' + (path.extname(base_file).split('.').pop())
+                output: path.dirname(dest + '/' + base_file) + '/' + this.props.title + '.' + (path.extname(base_file).split('.').pop())
             };
 
             // Rename the base files
             fse.rename(rename.input, rename.output, function(error) {
-                i++;
-
                 if(error) {
                     throw error;
                 }
 
+                queue++;
+
                 // Init the callback when all files have been renamed
-                if (i < (output_config.base_files.length)) {
+                if (queue < (output_config.base_files.length)) {
                     return;
                 }
 
-                if (typeof output_config.callback != 'function')
-                {
+                // Proceed if we have a callback defined
+                if (typeof output_config.callback != 'function') {
                     return;
                 }
 
-                output_config.callback(dest, module_name);
+                output_config.callback(dest, {
+                    title: title,
+                    template: template
+                });
             });
         }, this);
 
